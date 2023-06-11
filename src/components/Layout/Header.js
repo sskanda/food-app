2092168917397375b3e0db2353bced7392afd0ad
Skanda <sskanda98@gmail.com>
2023-06-11 22:38:23 +0530

    header added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import "../Layout/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <h1 id="headtext">Foodie</h1>
+        <div>
+          <button className="header-button">
+            <FontAwesomeIcon className="cart-icon" icon={faCartShopping} />
+            <span>Your Cart </span>
+            <span className="badge">3</span>
+          </button>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
